ffb3e6a39a91271025d1e966e9942e8ae2242147
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-22 20:20:27 -0300

    Update

--- app/src/main/java/com/example/appreceitas/view/Detalhe1Activity.kt
+++ app/src/main/java/com/example/appreceitas/view/Detalhe1Activity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appreceitas.R
 
-class DetalheActivity : AppCompatActivity() {
+class Detalhe1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detalhe)
+        setContentView(R.layout.activity_detalhe1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
